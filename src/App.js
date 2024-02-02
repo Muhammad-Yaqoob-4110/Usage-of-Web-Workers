bb358b82027fe9d905201797930a72529238567a
Muhammad-Yaqoob-4110 <my6580861@gmail.com>
2024-02-02 21:08:28 +0500

    add react page which have heavy data processing without webwrorks

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WebWorkers from './components/web-works';
+
+function App() {
+  return (
+    <div>
+      <WebWorkers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/web-works.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+export default function WebWorkers() {
+  const [count, setCount] = useState(0);
+  const [answer, setAnswer] = useState(0);
+
+  function getAnswerToLife() {
+    let sum = 0;
+    for (let i = 0; i < 5000000000; i++) {
+      sum += i;
+    }
+    setAnswer(42);
+  }
+
+  return (
+    <>
+      <div className="text-3xl flex flex-col gap-4 justify-center border h-screen items-center font-bold">
+      <h2>The answer to life is {answer}</h2>
+      <h4>The count is {count}</h4>
+        <div className="flex flex-row gap-4">
+          <button onClick={() => setCount(count + 1)} className="btn btn-active">+1</button>
+          <button onClick={getAnswerToLife} className="btn btn-active">Get Answer</button>
+        </div>
+      </div>
+    </>
+  );
+}
